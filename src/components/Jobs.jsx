8f4535858c4354ab93541d87fdbd996e4818bcff
Joe Cui <xiaozhou.cui@gmail.com>
2021-06-30 16:23:52 +1000

    Added positions in Strapi and Gatsby

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -6,7 +6,7 @@ import { Link } from 'gatsby'
 
 const query = graphql`
   {
-    allStrapiJobs {
+    allStrapiJobs(sort: { fields: created_at, order: DESC }) {
       nodes {
         position
         company
@@ -22,8 +22,49 @@ const query = graphql`
 
 const Jobs = () => {
   const data = useStaticQuery(query)
-  console.log(data)
-  return <h2>jobs component</h2>
+  const {
+    allStrapiJobs: { nodes: jobs },
+  } = data
+  const [value, setValue] = React.useState(0)
+  const { company, position, date, description } = jobs[value]
+  return (
+    <section className='section jobs'>
+      <Title title='experience' />
+      <div className='jobs-center'>
+        {/* btn container */}
+        <div className='btn-container'>
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={index}
+                className={index === value ? 'job-btn active-btn' : 'job-btn'}
+                onClick={() => setValue(index)}
+              >
+                {item.company}
+              </button>
+            )
+          })}
+        </div>
+        {/* job info */}
+        <article className='job-info'>
+          <h3>{position}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{date}</p>
+          {description.map(item => {
+            return (
+              <div key={item.id} className='job-desc'>
+                <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
+                <p>{item.name}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+      <Link to='/about' className='btn center-btn'>
+        more info
+      </Link>
+    </section>
+  )
 }
 
 export default Jobs
